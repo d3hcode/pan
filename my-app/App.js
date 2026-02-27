d259d9a0d4e936492addd4f6cfce79cdc1a352ad
d3hcode <dehschool123@gmail.com>
2026-02-27 13:34:47 +0000

    aula 02

--- my-app/App.js
+++ my-app/App.js
@@ -3,18 +3,37 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.contanier}>
+      <View style={estilos.area1}>
+        <Text style={estilos.font}>S</Text>
+        <Text style={estilos.font}>B</Text>
+          <Text style={estilos.font}>B</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const estilos = StyleSheet.create({
+  contanier:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#4955c4ff',
     justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
   },
+  font:{
+    fontSize: 80,
+    color: '#fff',
+    backgroundColor: '#3f9aa7ff',
+    margin: 10,
+    
+
+    
+  },
+  area1:{    
+    width: 370,
+    backgroundColor: '#135886ff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
 });
