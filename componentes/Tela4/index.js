ad28d01fb4fac866c74ae4d9daf854b720536a4b
d3hcode <dehschool123@gmail.com>
2026-05-29 11:57:32 +0000

    final aula 11

--- componentes/Tela4/index.js
+++ componentes/Tela4/index.js
@@ -1,8 +1,56 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Button, Image, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-export default function Tela4() {
-    return (
-        <Text>Tela 4 </Text>
-    );
+export default function TelaInicial (props) {
+  return (
+    <View style={styles.container}>
+      
+      <View style={styles.container02} >
+      <Text style={styles.textos}>SOBRE NÓS</Text>
+      <Text style={styles.textos}>
+        A Marial Coffee é uma cafeteria 
+        que tem como objetivo proporcionar uma 
+        experiência única aos amantes de café.</Text>
+      <Text style={styles.textos}>
+        Fundada em 2020, a Marial Coffee se destaca
+         por oferecer uma variedade de cafés especiais, 
+         preparados com grãos de alta qualidade e técnicas<Text style={styles.textos}> 
+         de barista. Além disso, a cafeteria valoriza a sustentabilidade, utilizando práticas eco-friendly e apoiando produtores locais. Com um ambiente acolhedor e moderno, a Marial Coffee é o lugar perfeito para desfrutar de um café delicioso e relaxar com amigos ou trabalhar em um espaço inspirador.
+      </Text>
+      </Text>
+        </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ede1d5",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+ container02: {
+    backgroundColor: "#412402",
+    padding: 20,
+    borderRadius: 10,
+    width: 500,
+    height: 500,
+
+    alignItems: "center",
+},
+
+  textos: {
+    color: "#ffffffff",
+    fontSize: 14,
+    fontFamily: "Playfair",
+    marginBottom: 20,
+    padding: 10,
+
+  },
+
+
+  
+});
+
--- componentes/TelaInicial/index.js
+++ componentes/TelaInicial/index.js
@@ -5,16 +5,16 @@ import { StyleSheet } from 'react-native';
 export default function TelaInicial (props) {
   return (
     <View style={styles.container}>
-      <Text>Eu amo caféeeeeee</Text>
+      <Image source={require("../../assets/bear.jpg")} style={{ width: 280, height: 280, marginBottom: 20, borderRadius: 900 }} />
       <Text style={styles.titulo}>Marial Coffee</Text>
 
       <Text style={styles.textos}>
-        Os principais filmes da franquia estão divididos em e trilogias. Clique no botão a seguir para conferir.
+        コーヒー
       </Text>
 
-      <Button title="Ver 1ª Trilogia" onPress = { () => props.navigation.navigate("Tela2") } color="#372d00" />
-      <Button title="Ver 2ª Trilogia" onPress = { () => props.navigation.navigate("Tela3") } color="#372d00" />
-      <Button title="Ver 3ª Trilogia" onPress = { () => props.navigation.navigate("Tela4") } color="#372d00" />
+      <Button title="PEDIR AGORA" onPress = { () => props.navigation.navigate("Tela2") } color="#412402" />
+      <Button title="      LOGIN      " onPress = { () => props.navigation.navigate("Tela3") } color="#412402" />
+      <Button title="      SOBRE      " onPress = { () => props.navigation.navigate("Tela4") } color="#412402" />
     </View>
   );
 }
@@ -22,20 +22,26 @@ export default function TelaInicial (props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F3E9DC",
+    backgroundColor: "#ede1d5",
     justifyContent: "center",
     alignItems: "center",
   },
 
   titulo: {
-    color: "#C08552",
-    fontSize: 24,
+    color: "#412402",
+    fontSize: 60,
     fontFamily: "Playfair",
-  },
+    padding: 10, },
 
   textos: {
-    color: "#C08552",
-    fontSize: 16,
+    color: "#493309ff",
+    fontSize: 30,
+    fontFamily: "Playfair",
+    marginBottom: 20,
+
   },
+
+
+  
 });
 
