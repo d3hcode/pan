ac11cf0d8bd7b49b56201de40c399519c90ac46a
d3hcode <dehschool123@gmail.com>
2026-05-29 13:30:09 +0000

    Atv-biblioteca

--- App.js
+++ App.js
@@ -1,29 +1,21 @@
-
 import React from "react";
- import { NavigationContainer } from "@react-navigation/native";
- import { createStackNavigator } from "@react-navigation/stack";
-
- import TelaInicial from "./componentes/TelaInicial";
- import Tela2 from "./componentes/Tela2";
- import Tela3 from "./componentes/Tela3";
- import Tela4 from "./componentes/Tela4";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { View } from "react-native-web";
-
- const Stack = createStackNavigator();
+import { Entypo, FontAwesome } from '@expo/vector-icons';
+ 
+const Stack = createStackNavigator();
 
  export default function App(){
   return(
-<NavigationContainer>
-<Stack.Navigator>
- 
- <Stack.Screen name="TelaInicial" component={TelaInicial}/>
-  <Stack.Screen name="Tela2" component={Tela2}/>
-  <Stack.Screen name="Tela3" component={Tela3}/>
-  <Stack.Screen name="Tela4" component={Tela4}/>
+    <View style={ estilos.container }>
+      <Text style={ estilos.texto }>Casa (home)</Text>
+      <Entypo name="home" size={ 24 } color="black" />
 
-</Stack.Navigator>
-  
-</NavigationContainer>
+      <Text style={ estilos.texto }>Usuário (user)</Text>
+      <FontAwesome name="user" size={ 24 } color="black" />
+    </View>
+
 
   );
  }
